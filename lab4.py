850a33d79a689935ac3c2c444edb40f3b1b17a34
SleepingCoding <167942050+SleepingCoding@users.noreply.github.com>
2025-03-02 23:07:49 -0500

    Preliminary opperations
    this is simply to connect the database API to my program so I can start on the program functionality.

--- lab4.py
+++ lab4.py
@@ -1,1 +1,14 @@
-print("Hello world!!!")
+import requests
+import random
+
+# Function to get trivia questions from OpenTDB
+def fetch_trivia_questions(amount=10):
+    url = f"https://opentdb.com/api.php?amount={amount}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data["results"]
+    else:
+        print("Error fetching trivia questions!")
+        return []
